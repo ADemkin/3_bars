f860c719657ec39ef39eb052a410b7735c97b808
antondemkin <antondemkin@yandex.ru>
2017-07-22 11:09:39 +0300

    Finally, done!

--- bars.py
+++ bars.py
@@ -11,24 +11,24 @@ def load_data(filepath):
 
 
 def get_biggest_bar(bar_data):
-    return max([bar for bar in bar_data], key=lambda bar:bar["SeatsCount"])
+    return max(bar_data, key=lambda bar:bar["SeatsCount"])
 
 
 def get_smallest_bar(bar_data):
-    return min([bar for bar in bar_data], key=lambda bar:bar["SeatsCount"])
+    return min(bar_data, key=lambda bar:bar["SeatsCount"])
 
 
 def get_vector_length(x1, y1, x2, y2):
     x = abs(float(x1) - float(x2))
     y = abs(float(y1) - float(y2))
-    return math.sqrt(x ** 2 + y ** 2)
+    return math.sqrt(x**2 + y**2)
 
 
 def get_closest_bar(bar_data, user_coordinate):
-    return min([bar for bar in bar_data], key=lambda bar:get_vector_length(user_coordinate[0],
-                                                                           user_coordinate[1],
-                                                                           bar['Latitude_WGS84'],
-                                                                           bar['Longitude_WGS84']))
+    return min(bar_data, key=lambda bar:get_vector_length(user_coordinate[0],
+                                                          user_coordinate[1],
+                                                          bar['Latitude_WGS84'],
+                                                          bar['Longitude_WGS84']))
 
 
 def print_bar_info(bar_size_string, bar_data):
@@ -44,7 +44,7 @@ def main():
     
     smallest_bar = get_smallest_bar(json_bar_data)
     print_bar_info('маленький', smallest_bar)
-    
+
     raw_coordinates = input("Введите координаты или скопируйте их из яндекс.карт в формате 11.111111, "
                             "22.222222:\n> ", )
     coordinates = raw_coordinates.split(',')
